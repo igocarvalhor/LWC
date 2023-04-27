3fdc3fd4136722bf40dd9732d60f2a98ba029b34
Igor Carvalho <106832918+igocarvalhor@users.noreply.github.com>
2023-04-26 22:30:20 -0300

    Create leadAtv1.js

---
+++ leadAtv1.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+
+export default class leadDetails extends LightningElement {
+
+lead = [
+    {cod: 0, nome: " Carlos", sobrenome: "Drummond", idade: "200", email: "alguma@email.com", classificacao:"HOT", empresa:"Autonomo", receitaanual:"R$452321"},
+    {cod: 1, nome: " Cora", sobrenome: "Coralina", idade: "100", email: "alguma@email.com", classificacao:"COLD",   empresa:"Autonomo", receitaanual:"R$8846546"},
+    {cod: 2, nome: " Steve", sobrenome: "Spielberg", idade: "70", email: "alguma@email.com", classificacao:"WARM",  empresa:"Hollywood", receitaanual:"R$878465"},
+    {cod: 3, nome: "Quentin", sobrenome: "Tarantino", idade: "60", email: "alguma@email.com", classificacao:"HOT", empresa:"Hollywood", receitaanual:"R$46546546"},
+    {cod: 4, nome: "Friozin", sobrenome: "Tarantino", idade: "60", email: "alguma@email.com", classificacao:"HOT", empresa:"Hollywood", receitaanual:"R$46546546"},
+  
+]
+
+
+
+
+}
